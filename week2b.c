0bea88de5b7c6d3cec1324e8dd39ce2f30c799b4
devanshbelwal0-sketch <devanshbelwal0@gmail.com>
2025-08-28 06:58:42 +0530

    Add files via upload

---
+++ week2b.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    int n, m;
+    printf("Enter rows and columns:\n");
+    scanf("%d%d", &n, &m);
+
+    int mat[n][m];
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    int row = -1;
+    int j = m - 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        while (j >= 0 && mat[i][j] == 1)
+        {
+            j--;
+            row = i;
+        }
+    }
+
+    if (row == -1)
+        printf("NO '1' IN ANY ROW\n");
+    else
+        printf("HIGHEST NUMBER OF ONE IN ROW %d\n", row + 1);
+
+    return 0;
+}
